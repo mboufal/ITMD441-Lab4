192dc9824f65f9a5ae6ef191111a9eda45251e09
mboufal <mboufal@hawk.iit.edu>
2022-11-20 14:48:46 -0600

    update script
    add dom elements

--- script.js
+++ script.js
@@ -9,6 +9,46 @@ window.addEventListener('load',() => {
 		//storing longitute and latitude in variables
 		long = position.coors.longitude;
 		lat = position.coords.latitude;
+		const base = `https://api.openweathermap.org/data/2.5/weatherlat=${lat}&lon=${long}&appid=${api}&units=metric`;
+		console.log(base);
+		
+		//fetching
+		fetch(base)
+			.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+		  const { temp } = data.main;
+		  const place = data.name;
+		  const { description, icon } = data.weather[0];
+		  const { sunrise, sunset } = data.sys;
+		  
+		  const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+		  const fahrenheit = (temp * 9) / 5 + 32;
+		  
+		  // Converting Epoch(Unix) time to GMT
+          const sunriseGMT = new Date(sunrise * 1000);
+          const sunsetGMT = new Date(sunset * 1000);
+		  
+		  //DOM interaction
+		  iconImg.src = iconUrl;
+		  loc.textContent = `${place}`;
+		  desc.textContent = `${description}`;
+		  tempC.textContent = `${temp.toFixed(2)} °C`;
+		  tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
+		  sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
+		  sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
+		  });
 		});
-	}
+      }
 });
+const iconImg = document.getElementById('weather-icon');
+const loc = document.querySelector('#location');
+const tempC = document.querySelector('.c');
+const tempF = document.querySelector('.f');
+const desc = document.querySelector('.desc');
+const sunriseDOM = document.querySelector('.sunrise');
+const sunsetDOM = document.querySelector('.sunset');
+
+
+
